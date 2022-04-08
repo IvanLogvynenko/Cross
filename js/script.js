be73a28f5d5648e289ad3882e9efb64cae615201
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-04-08 11:49:36 +0300

    ending

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-const cross = document.getElementById("cross");
+"use strict";
+let halfs = [];
+halfs = Array.prototype.concat.apply(halfs, 
+                                document.querySelector("#line1").children);
+halfs = Array.prototype.concat.apply(halfs, 
+                                document.querySelector("#line3").children);
+const middleLine = document.getElementById("middle");
+
+
+
+document.getElementById("cross").addEventListener("click", () => {
+    halfs.forEach(item =>{
+        if (!item.classList.contains("active"))
+        {
+            item.classList.add("active");
+		}
+        else
+        {
+            item.classList.remove("active");
+        }
+    });
+	if (!middleLine.classList.contains("active")) {
+		document.getElementById("middle").classList.add("active");
+	} else {
+		document.getElementById("middle").classList.remove("active");
+	}
+});
